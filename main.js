b70442f46ed2fd55561232c80ba9dd28f3ab6075
leul44 <leul441@hotmail.com>
2019-02-15 20:49:48 -0500

    js file updated

---
+++ main.js
@@ -0,0 +1,24 @@
+/*
+let name = "Leul"   ;
+let age = 22;
+ 
+if (age=>21){
+alert(name +" "+ "you are an adult")
+}else{
+alert(name +" " +"you need to grow up")
+
+
+}; 
+*/
+
+let speedlimit=35;
+let currentspeed=45;
+let speed
+
+if (speedlimit=>currentspeed){
+alert ("take the picture")}
+else{
+alert("do nothing ")
+}
+
+
